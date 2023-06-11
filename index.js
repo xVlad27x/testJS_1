c45d6ff7138d4226cebbde96e1aa5f1067e57195
Vladyslav <example@gmail.com>
2023-06-11 21:08:08 +0200

    First lesson

---
+++ index.js
@@ -0,0 +1,122 @@
+const arr = [5,5,5];
+
+// function ()) {
+
+// }
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+// // console.log(something);
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//     0: 'a',
+//     '1': 'b',
+//     2: 'c',
+//     abc: {
+//         df: [{}, {}],
+//         def: {
+
+//         }
+//     }
+// };
+// const b = 'b';
+
+// // arrObj.b = '1234';
+// arrObj[b] = '1234';
+// // console.log(arr[1]);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// // const obj = {a: 1, b: 2};
+// const obj = {
+//     Anna: 500,
+//     'Alice': 800
+// };
+
+// alert('Hi');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', '18');
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = +prompt('How old are you?', '');
+
+// document.write(typeof(answers));
+// console.log(typeof(answers));
+// console.log(typeof(null));   // ОФИЦИАЛЬНАЯ ОШИБКА ПОМНИ
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '5');
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Vlad';
+// alert(`Privet ${user}`);
+
+// console.log('arr' + ' - object');
+// console.log(4 + ' - object');
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+    
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === '8');
+
+// &&
+// ||
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 === 8);
+// console.log(2 + 2 * 2 !== 8);
+// console.log(2 + 2 * 2 != '6');
+// console.log(2 + 2 * 2 !== '6');
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+const numberOfFilms = +prompt('How many films heve you alreadu watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('One of the last movies you watched?', ''),
+      b = prompt('How much would you rate it?', ''),
+      c = prompt('One of the last movies you watched?', ''),
+      d = prompt('How much would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
