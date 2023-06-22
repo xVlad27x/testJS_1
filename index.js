40ed80669255a534faf630b3a0868057af77fcd6
Vladyslav <example@gmail.com>
2023-06-22 22:30:25 +0200

    trird commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const arr = [5,5,5];
+'use strict';
+
+// const arr = [5,5,5];
 
 // function ()) {
 
@@ -116,6 +118,598 @@ const arr = [5,5,5];
 // console.log(2 + 2 * 2 !== 8);
 // console.log(2 + 2 * 2 != '6');
 // console.log(2 + 2 * 2 !== '6');
+
+// if (4 == 9) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too much');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// const num2 = 101;
+
+// switch (num2) {
+// case 99:
+//     console.log('Wrong');
+//     break;
+// case 150:
+//     console.log('Too much');
+//     break;
+// case 100:
+//     console.log('Ok');
+//     break;
+// default:
+//     console.log('Not this time');
+//         // break;
+// }
+
+// const hamburger = true,
+//     fries = false;
+
+// if (hamburger && fries) {
+//     console.log('Я сыт!');
+// } 
+
+// console.log((hamburger && fries));
+
+// const hamburger = 3,
+//     fries = 1,
+//     cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'sknjns');
+
+
+
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// const hamburger = 3,
+//     fries = 0,
+//     cola = 0;
+
+// if (hamburger || cola || fries) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// const hamburger = 0,
+//     fries = null,
+//     cola = 0;
+
+// if (hamburger || cola || fries) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// console.log(hamburger || cola || fries);
+
+// let johnReport, alexReport, sameReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || sameReport || mariaReport);
+
+// const hamburger = 3,
+//     fries = 3,
+//     cola = 0,
+//     nuggets = 2;
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+// const hamburger = 3,
+//     fries = 3,
+//     cola = 0,
+//     nuggets = 2;
+
+// if (hamburger === 3 && (cola === 2 || fries === 3) && nuggets) {
+//     console.log('Все довольны!');
+// } else {
+//     console.log('Мы уходим!');
+// }
+
+// console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
+
+// console.log(!0);
+
+// console.log( NaN || 2 || undefined );   // 2
+// console.log( NaN && 2 && undefined );  // Nan
+// console.log( 1 && 2 && 3 );  // 3
+// console.log( !1 && 2 || !3 ); // false
+// console.log( 1 && 2 || !3 ); // 2
+// console.log( !1 && 2 || 3 ); // 3
+// console.log( 25 || null && !3 ); // 25
+// console.log( NaN || null || !3 || undefined || 5); // 5
+// console.log( NaN || null && !3 && undefined || 5); // 5
+// console.log( 5 === 5 && 3 > 1 || 5); // true
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!');
+// } else {
+//     console.log('а х там');
+// }
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// Фигурные скобки не требуются для тела цикла из одной строки
+
+// Если тело цикла состоит лишь из одной инструкции, мы можем опустить фигурные скобки {…}:
+// let num = 50;
+
+// while (num <= 55) console.log(num++);
+
+
+// let num = 50;
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+
+// let num = 50;
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 3; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);  // 4 6 8
+//     }
+// }
+
+// for (let i = 3; i < 10; i++) {
+//     if (i % 2 === 0) continue; {
+//         console.log(i);  // 3 5 7 9
+//     }
+// }
+
+// let num;
+
+// do {
+//     num = prompt('Введи число больше 100?', 0);
+// } while (num <= 100 && num);
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// let result = '';
+// const lenght = 7;
+
+// for (let i = 1; i < lenght; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) break;
+//     console.log(i);
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//     console.log(i);
+// }
+
+// let i = 2;
+// while (i <= 16) {
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// const arrayOfNumbers = [];
+
+// for (let i = 5; i < 11; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+
+// console.log(arrayOfNumbers);
+// return arrayOfNumbers;
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
+
+// function showFirstMessage() {
+//     console.log('Hi man!');
+// }
+
+// showFirstMessage();
+
+
+// function showFirstMessage(text) {
+//     console.log(text);
+// }
+
+// showFirstMessage('Hi man!');
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 21;
+// }
+
+// showFirstMessage('Hi man!');
+// console.log(num);
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hi man!');
+// console.log(num);
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// showFirstMessage('Hi man!');
+// console.log(num);
+
+
+// function calc(a, b) {
+//     return (a + b);
+//     // console.log('sdfds');  // unreachable code - мёртвый код, который никогда не выполнится. после return мы ничего не задаём
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function calc(a, b) {
+//     return (a + b);
+//     // console.log('sdfds');  // unreachable code - мёртвый код, который никогда не выполнится. после return мы ничего не задаём
+// }
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+// logger();
+
+
+// const calc = (a, b) => a + b;
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+
+// const calc = (a, b) => { return a + b };
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// }
+
+// console.log(calc(40, 7));
+
+
+// let a = 3;
+// function addTwo(x) {
+//     let ret = x + 2;
+//     return ret;
+// }
+// let b = addTwo(a);
+// console.log(b);
+
+
+// function convert (amount) {
+//     console.log(28 * amount);
+// }
+// convert(500);
+
+
+// const usdCurr = 28;
+
+// function convert(amount) {
+//     console.log(usdCurr * amount);
+// }
+// convert(500);
+
+
+// const usdCurr = 28;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+
+
+// функцию нужно создавать универсальную (смотри снизу пример)!!!
+
+// const usdCurr = 28;
+// const euroCurr = 31;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, euroCurr);
+
+
+// const numFirst = 5;
+// const numSecond = 10;
+
+// function count(a, b) {  // аргументы
+//     console.log(a + b);
+// }
+
+// count(numFirst, numSecond);  // вызов функции
+
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// promotion(convert(500, usdCurr));
+
+// тоже самое внизу, только с промежуточной переменной
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return;  // после return функци прекратила свою работу / return вернёт undefined
+//     }
+//     console.log('Done');  // сюда уже код не дошёл
+// }
+
+// test(); // 0 1 2 3
+
+
+// function doNothing() {};
+// console.log(doNothing() === undefined);  // true
+
+// console.log('12'); // 12 undefined
+// console.log(); // - это функция / console - это объект большой / .log - это его метод (т.е. функция, которая записана внутри объекта) т.к. это функция, и она должна что-то возвращать, то и возвращает undefined / любая функция, которая не содержит return будет выдавать undefined
+
+
+// function sayHello(name) {
+//     return 'Hi, ' + name;
+// }
+
+// console.log(sayHello('Vlad'));
+
+
+// function sayHello2(name) {
+//     return `Привет, ${name}!`;
+// }
+
+// console.log(sayHello2('Alex'));
+
+
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num,  num + 1];
+// }
+// console.log(returnNeighboringNumbers(5));
+
+
+// function getMathResult(num, times) {
+//     if (typeof(times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`;
+//         } else {
+//             str += `${num * i}---`;
+//         }
+//     }
+
+//     return str;
+// }
+
+// console.log(getMathResult(10, 5));
+
+
+// const str = 'test';
+// console.log(str.length);
+// console.log(str[2]);
+
+
+// const arr = [1, 2, 4];
+// console.log(arr.length);
+
+
+// const str = 'test';
+// console.log(str.toUpperCase()); // Возвращает новое значение
+// console.log(str); // А тут оно остаётся старое
+
+
+console.dir(Number);  // в браузере смотри
+Math.  // в браузере смотри
+
+
+const str = 'teSt';
+console.log(str.toLowerCase());
+
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit')); // Поиск под строки
+console.log(fruit.indexOf('q'));
+
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6, 11)); // 6 - начало строки 11 - то что нам нужно, только не включая
+console.log(logg.slice(6)); // с 6 и просто до конца
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));  // не поддерживает минус -
+console.log(logg.substr(6, 2));  // сколько символов нужно вырезать
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));  // превращает строку в число 12
+console.log(parseFloat(test));  // 12.2
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 'use strict';
-const numberOfFilms = +prompt('How many films heve you alreadu watched?', '');
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films heve you alreadu watched?', '');
+ 
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films heve you alreadu watched?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,15 +20,109 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of the last movies you watched?', ''),
-      b = prompt('How much would you rate it?', ''),
-      c = prompt('One of the last movies you watched?', ''),
-      d = prompt('How much would you rate it?', '');
+// const a = prompt('One of the last movies you watched?', ''),
+//     b = prompt('How much would you rate it?', ''),
+//     c = prompt('One of the last movies you watched?', ''),
+//     d = prompt('How much would you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// Вариант с for -- начало
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('One of the last movies you watched?', ''),
+//         b = prompt('How much would you rate it?', '');
 
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
 
+// }
+// Вариант с for -- конец
 
+// Вариант с while -- начало
+// let num = 0;
+
+// while (num < 2) {
+//     const a = prompt('One of the last movies you watched?', ''),
+//         b = prompt('How much would you rate it?', '');
+//     num++;
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         num--;
+//     }
+// }
+// Вариант с while -- конец
+
+// Вариант с do while -- начало
+// let num = 0;
+// do {
+//     const a = prompt('One of the last movies you watched?', ''),
+//         b = prompt('How much would you rate it?', '');
+//     num++;
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         num--;
+//     }
+// }
+// while (num < 2);
+// Вариант с do while -- конец
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('One of the last movies you watched?', ''),
+            b = prompt('How much would you rate it?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Мало смотришь фильмецов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Норм смотришь фильмецов');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Ты задрот');
+    } else {
+        console.log('Ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
